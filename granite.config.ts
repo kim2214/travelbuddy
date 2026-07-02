aa1bf33c9ee8c05ecd5ef55c30f29e7e32f8638e
kim2214 <anzel21@naver.com>
2026-07-02 10:10:34 +0900

    granite.config.ts 앱 아이콘 등록

--- granite.config.ts
+++ granite.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   brand: {
     displayName: "여행친구", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: "#FD9B3C", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: "", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+    icon: "https://static.toss.im/appsintoss/53765/c9fb1bd6-f85b-4725-a2e3-c4aa2a7e759b.png", // 콘솔에 등록한 아이콘과 동일한 이미지 주소
+
   },
   web: {
     host: "localhost",
